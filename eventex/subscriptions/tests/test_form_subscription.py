6a59879e6596ce006d1be3f9990a21c2af4a8ab9
unknown <ogvinicius@gmail.com>
2018-07-03 16:40:09 -0300

    Refatora geral

---
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from eventex.subscriptions.forms import SubscriptionForm
+
+
+class SubscriptionFormTest(TestCase):
+    def setUp(self):
+        self.form = SubscriptionForm()
+
+
+    def test_form_has_fileds(self):
+        """Form mus have 4 fields"""
+
+        expected = ['name', 'cpf', 'email', 'phone']
+        self.assertSequenceEqual(expected, list(self.form.fields))
